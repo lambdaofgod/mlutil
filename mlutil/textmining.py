31098c7fa2ed71a2cf874ecb796cb92821fcaa16
Jakub Bartczuk <bartczukkuba@gmail.com>
2018-10-04 23:19:03 +0200

    refactored text vectorization

--- mlutil/textmining.py
+++ mlutil/textmining.py
@@ -2,18 +2,34 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import OneHotEncoder, MultiLabelBinarizer, LabelEncoder
 
 
-def vectorize_text_df(examples_df, vectorized_column, target_column, vectorizer=CountVectorizer, **kwargs):
+def prepare_text_modeling_df(examples_df, vectorized_column, target_colum, vectorizer=CountVectorizer, **kwargs):
+    data, transformers = vectorize_text_df(examples_df, vectorized_column, vectorizer, **kwargs)
+    target_data, target_transformers = encode_target(examples_df, target_column)
+    return {**data, **target_data}, {**transformers, **target_transformers}
 
-    vectorizer = vectorizer(**kwargs)
+
+def vectorize_text_df(examples_df, vectorized_column,  vectorizer=CountVectorizer(), **kwargs):
+
     features = vectorizer.fit_transform(examples_df[vectorized_column])
 
+    return ({'features': features},
+            {'vectorizer': vectorizer})
+
+
+def encode_target(examples_df, target_column):
     le = LabelEncoder()
     ohe = OneHotEncoder()
     labels = le.fit_transform(examples_df[target_column]).reshape(-1, 1)
     labels_ohe = ohe.fit_transform(labels).todense()
+    
     vectorized_data = {
-        'features': features,
         'labels': labels,
-        'labels_onehot' : labels_ohe
+        'labels_onehot': labels_ohe
     }
-    return vectorized_data, (vectorizer, ohe, le)
+     
+    transformers = {
+        'label_encoder': le,
+        'onehot_encoder': ohe
+    }
+    return vectorized_data, transformers
+
