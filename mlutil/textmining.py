d264fe783347f6012cc8973c8640e1f11946e347
Jakub Bartczuk <bartczukkuba@gmail.com>
2019-04-11 00:58:26 +0200

    word similarity with wordnet

--- mlutil/textmining.py
+++ mlutil/textmining.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import OneHotEncoder, MultiLabelBinarizer, LabelEncoder
 
+from nltk.corpus import wordnet as wn
+from nltk.corpus import wordnet_ic
+
 
 def prepare_text_modeling_df(examples_df, vectorized_column, target_colum, vectorizer=CountVectorizer, **kwargs):
     data, transformers = vectorize_text_df(examples_df, vectorized_column, vectorizer, **kwargs)
@@ -34,6 +37,10 @@ def encode_target(examples_df, target_column):
     }
     return vectorized_data, transformers
 
+################
+# TOPIC MODELING
+################
+
 
 def top_topic_words(model, feature_names, n_words=5):
     """
@@ -47,3 +54,19 @@ def top_topic_words(model, feature_names, n_words=5):
     topic_words_dict = {'topic_' + str(topic_idx): top_words(topic) for topic_idx, topic in enumerate(model.components_)}
     
     return pd.DataFrame(topic_words_dict).T
+
+##################
+# WORD SIMILARITY
+##################
+
+
+def get_wordnet_similarity(word, another_word, similarity_method='resnik', ic=wordnet_ic.ic('ic-semcor.dat')):
+    assert similarity_method in ['lin', 'jcn', 'resnik'], 'Unsupported similarity method: ' + str(similarity_method)
+    word_synset = wn.synsets(word, wn.NOUN)[0]
+    another_word_synset = wn.synsets(another_word, wn.NOUN)[0]
+    if similarity_method == 'lin':
+        return word_synset.lin_similarity(another_word_synset, ic)
+    elif similarity_method == 'jcn': 
+        return word_synset.jcn_similarity(another_word_synset, ic)
+    else:
+        return word_synset.res_similarity(another_word_synset, ic)
