4c8e71d68248bb0de38ea4a3f195ec82ac5bed6f
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-05-29 17:58:18 +0200

    one hot encoder column transformer

--- mlutil/misc.py
+++ mlutil/misc.py
@@ -1,2 +1,18 @@
+import pandas as pd
+from sklearn import compose, preprocessing
+
+
+def one_hot_encoder_column_transformer(columns):
+    """
+    transformer that stacks outputs of one-hot encoders for specified columns
+    """
+    return compose.ColumnTransformer(
+        [
+            (col, preprocessing.OneHotEncoder(), [col])
+            for col in columns
+        ]
+    )
+
+
 def make_gzipped_csv(data, name, dtype='float16'):
     pd.DataFrame(data.astype(dtype)).to_csv(name + '.csv.gz')
