45fb3031c38ae0e6ada4e48b6681ba88f7881c0b
lambdaofgod <bartczukkuba@gmail.com>
2021-05-18 20:23:53 +0200

    utils: maybe retrieve objects if are stored

--- mlutil/utils.py
+++ mlutil/utils.py
@@ -17,6 +17,12 @@ class maybe_csv_writer:
         else:
             logging.warning("{} already exists".format(self.csv_path))
 
+    def get_csv_if_not_exists(self, get_df):
+        if not os.path.exists(self.csv_path):
+            return get_df()
+        else:
+            return pd.read_csv(self.csv_path)
+
     def __exit__(self, *args):
         pass
 
@@ -32,10 +38,16 @@ class maybe_pickler:
 
     def write_pickle_if_not_exists(self, get_object):
         if not os.path.exists(self.pickle_path):
-            with open(self.pickle_path, 'wb') as f:
                 self.pickler.dump(get_object(), f)
         else:
             logging.warning("{} already exists".format(self.pickle_path))
+
+    def get_pickle_if_exists(self, get_object):
+        if not os.path.exists(self.pickle_path):
+            return get_object()
+        else:
+            with open(self.pickle_path, 'rb') as f:
+                return pickle.load(f)
 
     def __exit__(self, *args):
         pass
