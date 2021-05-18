28d6b65d7774840c1d577ff323acf272d1dfcb59
lambdaofgod <bartczukkuba@gmail.com>
2021-05-18 20:13:21 +0200

    utilities for storing stuff if it is not already stored

--- mlutil/__init__.py
+++ mlutil/__init__.py
@@ -1,3 +1,3 @@
 from .feature_extraction.embeddings import load_gensim_embedding_model
 from .topic_modeling import top_topic_words, topic_coherence
-from .utils import maybe_csv_writer
+from .utils import maybe_csv_writer, maybe_pickler
--- mlutil/utils.py
+++ mlutil/utils.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import pickle
 
 
 class maybe_csv_writer:
@@ -9,11 +11,31 @@ class maybe_csv_writer:
     def __enter__(self):
         return self
 
-    def write_if_not_exists(self, df):
+    def write_csv_if_not_exists(self, get_df):
         if not os.path.exists(self.csv_path):
-            df.to_csv(self.csv_path)
+            get_df().to_csv(self.csv_path)
         else:
             logging.warning("{} already exists".format(self.csv_path))
+
+    def __exit__(self, *args):
+        pass
+
+
+class maybe_pickler:
+
+    def __init__(self, pickle_path, pickler=pickle):
+        self.pickle_path = pickle_path
+        self.pickler = pickle
+
+    def __enter__(self):
+        return self
+
+    def write_pickle_if_not_exists(self, get_object):
+        if not os.path.exists(self.pickle_path):
+            with open(self.pickle_path, 'wb') as f:
+                self.pickler.dump(get_object(), f)
+        else:
+            logging.warning("{} already exists".format(self.pickle_path))
 
     def __exit__(self, *args):
         pass
