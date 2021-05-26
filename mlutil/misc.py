c5eb3265a682f489ec9fe6a585c2d8a0ff7f8915
lambdaofgod <bartczukkuba@gmail.com>
2021-05-26 21:51:12 +0200

    util for showing square image from vector

--- mlutil/misc.py
+++ mlutil/misc.py
@@ -1,5 +1,15 @@
+import numpy as np
 import pandas as pd
 from sklearn import compose, preprocessing
+import matplotlib.pyplot as plt
+
+
+def display_img_vector(img_vector, cmap='gray'):
+    vector_size = img_vector.shape[0]
+    img_size = int(np.sqrt(vector_size))
+    assert img_size ** 2 == vector_size
+    img = img_vector.reshape(img_size, img_size)
+    plt.imshow(img, cmap=cmap)
 
 
 def one_hot_encoder_column_transformer(columns):
