29e52fb16f78c6ea3aff4b2d16b3d1ee1f3925ac
lambdaofgod <bartczukkuba@gmail.com>
2022-02-20 23:15:03 +0100

    additional torch image vectorizer utils

--- mlutil/feature_extraction/images.py
+++ mlutil/feature_extraction/images.py
@@ -5,7 +5,7 @@ from torch import nn
 import tqdm
 from PIL import Image
 from typing import Union, List
-
+from torch.utils import data
 
 imagenet_normalize = transforms.Normalize(
     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
@@ -16,8 +16,9 @@ class TorchFeatureExtractor:
     def __init__(
         self,
         model,
-        last_nested_index=None,
-        last_layers=[nn.Flatten()],
+        appended_modules: List[nn.Module] = [nn.Flatten()],
+        last_layer_index: int = None,
+        last_nested_layer_index: int = None,
         use_gpu=True,
         normalize=imagenet_normalize,
         to_fp16=False,
@@ -26,9 +27,17 @@ class TorchFeatureExtractor:
         self.use_gpu = use_gpu
         self.normalize = normalize
         self.to_fp16 = to_fp16
-        self.model = self.get_layers(model, last_nested_index, last_layers)
+        self.model = self.get_layers(
+            model,
+            appended_modules=appended_modules,
+            last_nested_layer_index=last_layer_index,
+            last_layer_index=last_layer_index,
+        )
         self.scaler = transforms.Scale((img_size, img_size))
+        self.torch_transforms = [transforms.ToTensor(), self.scaler, self.normalize]
 
+    def torch_image_transform(self, img: Image.Image):
+        return self.normalize(transforms.ToTensor()(self.scaler(img)))
 
     def load_img_tensor(self, path):
         img = Image.fromarray(path)
@@ -36,8 +45,10 @@ class TorchFeatureExtractor:
 
     def process_img(self, img: Union[np.ndarray, Image.Image]):
         if type(img) is np.ndarray:
+            if img.dtype is not np.dtype("uint8"):
+                img = (img * 255).astype('uint8')
             img = Image.fromarray(img)
-        torch_img = self.normalize(transforms.ToTensor()(self.scaler(img)))
+        torch_img = self.torch_image_transform(img)
         return self.maybe_to_cuda(torch_img.unsqueeze(0))
 
     def maybe_to_cuda(self, torch_object):
@@ -56,15 +67,28 @@ class TorchFeatureExtractor:
             features.append(img_features.cpu().numpy())
         return np.vstack(features)
 
+    def get_features_from_dataset(
+        self, images_dset: data.Dataset, batch_size=32, use_tqdm=True
+    ):
+        tqdm_wrapper = tqdm.tqdm if use_tqdm else lambda x: x
+        data_loader = data.DataLoader(images_dset, batch_size=batch_size)
+        features = []
+        for batch in tqdm_wrapper(data_loader):
+            img_features = self.model(batch)
+            features.append(img_features.cpu().numpy())
+        return features
+
     def get_images(self, paths):
         return [self.load_img(p) for p in tqdm.tqdm(paths)]
 
-    def get_layers(self, model, last_nested_index, last_layer_modules):
-        modules = list(model.children())[:-1]
-        if last_nested_index is not None:
+    def get_layers(
+        self, model, appended_modules, last_layer_index, last_nested_layer_index
+    ):
+        modules = list(model.children())[:last_layer_index]
+        if last_nested_layer_index is not None:
             last_module = list(model.children())[-1]
-            modules.append(last_module[:last_nested_index])
-        model = nn.Sequential(*modules + last_layer_modules)
+            modules.append(last_module[:last_nested_layer_index])
+        model = nn.Sequential(*(modules + appended_modules))
         for p in model.parameters():
             p.requires_grad = False
         model.eval()
