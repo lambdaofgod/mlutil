f01203d19019f880328eb20b07f7994e4432c8f3
lambdaofgod <bartczukkuba@gmail.com>
2023-05-14 12:12:56 +0200

    hf rwkv model in minichain utils

--- mlutil/minichain_utils.py
+++ mlutil/minichain_utils.py
@@ -8,6 +8,7 @@ from transformers import (
     AutoTokenizer,
 )
 from typing import Union
+import torch
 
 
 class HuggingFaceLocalModel(BaseModel, Backend):
@@ -17,27 +18,29 @@ class HuggingFaceLocalModel(BaseModel, Backend):
     ]
 
     @classmethod
-    def load_model(cls, model_name, **kwargs):
-        try:
-            model = AutoModelForCausalLM.from_pretrained(model_name, **kwargs)
-        except OSError:
-            return AutoModelForSeq2SeqLM.from_pretrained(model_name, **kwargs)
-
-        return model
-
-    @classmethod
     def load(cls, model_name: str, device=0, **kwargs):
         tokenizer = AutoTokenizer.from_pretrained(model_name)
+        if "rwkv" in model_name:
+            from transformers import RwkvForCausalLM
+
+            model_cls = RwkvForCausalLM
+        else:
+            model_cls = AutoModelForCausalLM
+        model = model_cls.from_pretrained(
+            model_name,
+            torch_dtype=torch.float16,
+        )
         pipeline = pipelines.pipeline(
             task="text-generation",
             tokenizer=tokenizer,
-            model=cls.load_model(model_name, **kwargs),
+            model=model,
             device=device,
+            **kwargs,
         )
         return HuggingFaceLocalModel(model_name=model_name, pipeline=pipeline)
 
     def run(self, request: str) -> str:
-        return self.pipeline(request)[0]["generated_text"]
+        return self.pipeline(request, return_full_text=False)[0]["generated_text"]
 
     class Config:
         arbitrary_types_allowed = True
@@ -48,13 +51,12 @@ class RWKVModel(BaseModel, Backend):
     pipeline: rwkv_utils.RWKVPipelineWrapper
 
     @classmethod
-    def load(cls, model_name: str):
-        pipeline = rwkv_utils.RWKVPipelineWrapper.load(
-            model_path=model_name,
-        )
+    def load(cls, model_name: str, **kwargs):
+        pipeline = rwkv_utils.RWKVPipelineWrapper.load(model_path=model_name, **kwargs)
         return RWKVModel(model_name=model_name, pipeline=pipeline)
 
     def run(self, request: str) -> str:
+        print("request", request)
         return self.pipeline.generate(request)
 
     class Config:
